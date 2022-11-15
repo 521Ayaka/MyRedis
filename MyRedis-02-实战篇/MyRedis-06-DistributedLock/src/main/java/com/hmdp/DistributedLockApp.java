453864b56203cc27420594f3a1d003ff2a88ac63
尴尬酱 <2282514478@qq.com>
2022-11-15 15:52:02 +0800

    Redis学习-提交

---
+++ MyRedis-02-实战篇/MyRedis-06-DistributedLock/src/main/java/com/hmdp/DistributedLockApp.java
@@ -0,0 +1,32 @@
+package com.hmdp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+//暴露动态代理
+@EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.hmdp.mapper")
+@SpringBootApplication
+public class DistributedLockApp {
+
+    public static void main(String[] args) {
+        System.out.println("神里绫华");
+        SpringApplication.run(DistributedLockApp.class, args);
+    }
+
+}
+
+
+/**
+ *
+ *
+ *
+ * 2022/09/29 03:22:27 [error] 8479#8479:
+ * *1 open() "/usr/share/nginx/html/hmdp/js/axios.min.map" failed
+ * (2: No such file or directory),
+ * client: 127.0.0.1, server: localhost,
+ * request: "GET /js/axios.min.map HTTP/1.1",
+ * host: "localhost:8080"
+ */
